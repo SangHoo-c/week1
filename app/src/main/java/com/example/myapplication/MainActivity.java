932d75a3560cdc47a2ace6831726865d1db1357c
SangHoo-c <uphoo96@gmail.com>
2019-12-27 13:36:47 +0900

    12.27 -- first project

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,23 +1,13 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DividerItemDecoration;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+
 import android.widget.TabHost;
 
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    private ArrayList<Dictionary> mArrayList;
-    private CustomAdapter mAdapter;
-    private int count = -1;
 
 
 
@@ -48,40 +38,6 @@ public class MainActivity extends AppCompatActivity {
         ts3.setIndicator("TAB 3");
         tabHost1.addTab(ts3);
 
-
-
-
-        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview_main_list);
-        LinearLayoutManager mLinearLayoutManager = new LinearLayoutManager(this);
-        mRecyclerView.setLayoutManager(mLinearLayoutManager);
-
-
-        mArrayList = new ArrayList<>();
-
-        mAdapter = new CustomAdapter( mArrayList);
-        mRecyclerView.setAdapter(mAdapter);
-
-
-        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(mRecyclerView.getContext(),
-                mLinearLayoutManager.getOrientation());
-        mRecyclerView.addItemDecoration(dividerItemDecoration);
-
-
-
-        Button buttonInsert = (Button)findViewById(R.id.button_main_insert);
-        buttonInsert.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                count++;
-
-                Dictionary data = new Dictionary(count+"","Apple" + count, "사과" + count);
-
-                //mArrayList.add(0, dict); //RecyclerView의 첫 줄에 삽입
-                mArrayList.add(data); // RecyclerView의 마지막 줄에 삽입
-
-                mAdapter.notifyDataSetChanged();             }
-        });
 
     }
 }
